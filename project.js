f7e0ddf40a133f32acb99a9dbfb13795321c5f8c
POPORIii-39 <popo.39.pupuri@gmail.com>
2025-03-30 16:41:11 +0900

    スライダーバー追加

--- project.js
+++ project.js
@@ -1,10 +1,18 @@
+let bgColor = 0;
+
 function setup() {
-    let canvas = createCanvas(600, 600); // 左側に合うサイズ
-    canvas.parent("left-container"); // `#left-container` の中に配置
+    let canvas = createCanvas(600, 600);
+    canvas.parent("left-container");
 }
 
 function draw() {
-    background(0);
+    let slider = document.getElementById("colorSlider");
+    
+    slider.addEventListener("input", function () {
+        bgColor = Number(slider.value);
+    });
+
+    background(bgColor);
     let x = frameCount % width;
     let y = height / 2 + sin(frameCount * 0.1) * 100;
     fill(100, 200, 200);
