eb1cd1ca90987981fe9dd402aa24a652ac331d4a
POPORIii-39 <popo.39.pupuri@gmail.com>
2025-03-30 17:23:32 +0900

    サイズ変更

--- project.js
+++ project.js
@@ -1,13 +1,13 @@
 let bgColor = 0;
 
 function setup() {
-    let canvas = createCanvas(600, 600);
+    let canvas = createCanvas(windowWidth * 0.5, windowHeight * 0.5);
     canvas.parent("left-container");
 }
 
 function draw() {
     let slider = document.getElementById("colorSlider");
-    
+
     slider.addEventListener("input", function () {
         bgColor = Number(slider.value);
     });
@@ -17,4 +17,8 @@ function draw() {
     let y = height / 2 + sin(frameCount * 0.1) * 100;
     fill(100, 200, 200);
     ellipse(x, y, 20, 20);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth * 0.5, windowHeight * 0.5);
 }
